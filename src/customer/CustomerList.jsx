59844bce0a9ae8088644b8c462d2a4c3f49d3b24
Jordi <jordi.pastor@sky.uk>
2024-10-10 23:59:07 +0200

    Jordi Pastor solucio kata

--- src/customer/CustomerList.jsx
+++ src/customer/CustomerList.jsx
@@ -1,19 +1,50 @@
-const CustomerList = ({ customers }) => {
+import { useEffect, useState } from 'react';
+import Alert from './Alert';
+
+const CustomerList = ({ customers, setHomeAlertText, setHomeAlertVisible }) => {
+  const [showCustomerAlert, setShowCustomerAlert] = useState(false);
+  const showHomeAlert = (timeout, text)=> {
+    setHomeAlertText(text);
+    setHomeAlertVisible(true);
+    setTimeout(() => {
+      setHomeAlertVisible(false);
+    }, timeout);
+  };
+
+  useEffect(() => {
+    if (customers.length > 0) {
+      setShowCustomerAlert(true);
+      setTimeout(() => {
+        setShowCustomerAlert(false);
+      }, 2000);
+    }
+  }, [customers])
+
+  useEffect(() => {
+    showHomeAlert(3000, "Welcome to the Client List")
+    return ()=> {
+      showHomeAlert(3000, "Goodbye")
+    }
+  }, []);
+
   return (
-    <ul className="customer-list">
-      {
-        customers.map(customer => {
-          return (
-            <li key={customer.email}>
-              <strong>{customer.name}</strong><br/>
-              <span>Email: {customer.email}</span><br/>
-              <span>Phone: {customer.phone}</span><br/>
-              <span>Address: {customer.address}</span><br/>
-            </li>
-          )
-        })
-      }
-    </ul>
+    <>
+      <Alert visible={showCustomerAlert} text={"New client has been added"}/>
+      <ul className="customer-list">
+        {
+          customers.map(customer => {
+            return (
+              <li key={customer.email}>
+                <strong>{customer.name}</strong><br/>
+                <span>Email: {customer.email}</span><br/>
+                <span>Phone: {customer.phone}</span><br/>
+                <span>Address: {customer.address}</span><br/>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </>
   );
 };
 
